7c865dfaf1df8e1f13a35a7c2a9bc63bee72cfba
asa-o <asao.develop@gmail.com>
2024-08-19 10:24:41 +0900

    画像をindexedDBへ保存

--- dl-scraping/webapp/src/services/effectService.ts
+++ dl-scraping/webapp/src/services/effectService.ts
@@ -1,5 +1,10 @@
+"use client";
+
 import { EffectInfo } from "../types/effects";
 import { ResponseGetEffectList } from "../types/responses";
+import LocalDB from "@/stores/localDb";
+
+const sprintf = require("sprintf-js").sprintf;
 
 class EffectService {
   async getList(email: string, password: string, progressCallback: (progress: EffectInfo[]) => void): Promise<void> {
@@ -22,17 +27,41 @@ class EffectService {
       });
       const responseData: ResponseGetEffectList = await response.json();
       this.renewalPossesionList(responseData.effects);
+      this.downloadImage(responseData.effects);
       pagerNextExists = responseData.isNext;
       page++;
       progressCallback(responseData.effects);
     }
   }
 
+  downloadImage(effects: EffectInfo[]): void {
+    try {
+      effects.map(async (effect) => {
+        const response = await fetch("http://localhost:8081/get-effect-image", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ effectId: effect.Id }),
+        });
+
+        const data = await response.json();
+        if (data.succeed) {
+          const base64Response = await fetch(`data:image/jpeg;base64,${data.image}`);
+          const blob = await base64Response.blob();
+          LocalDB.getInstance().effectImage.add({ id: effect.Id, image: blob });
+        }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   renewalPossesionList(effects: EffectInfo[]): void {}
+
+  async getAllImages() {
+    return LocalDB.getInstance().effectImage.getAllDatas();
+  }
 }
 
 export default new EffectService();
-
-///
-// page nextがなくなるまで取得したデータはtempデータデーブルへ保持しておき、
-// nextがなくなったタイミングで逆順にして正規のテーブルへ移動する
